95f92c55fd1f646a0a259be7d2f43898f16e8001
KP670 <76938778+KP670@users.noreply.github.com>
2023-08-21 11:26:12 +1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Auto Dir
- 
+<h1>Goal</h1>
+Create a script that creates mutliple directories given the path name, file name, and count.
---
+++ cli/main.py
@@ -0,0 +1,48 @@
+## This is is for the context menu version
+
+import os
+
+def set_folder_name():
+    folder_name = input("Enter the name of the folder: ")
+    return folder_name
+
+def num_folders_to_create():
+    # Upon ValueError, perform a recursion
+    try:
+        num_folders = int(input("How many folders do you want to create? "))
+        
+        # Perform a recursion when the number of folders is negative
+        if num_folders < 0:
+            print("Please enter a positive integer")
+            num_folders = None
+        
+            return num_folders_to_create()
+        
+        else:
+            return num_folders  
+    
+    except ValueError:
+        print("Please enter a positive integer")
+        num_folders = None
+        
+        return num_folders_to_create()
+    
+
+def create_folders(folder_name, num_folders):
+    i = 0    
+    
+    # Create folders until num_folders is reached
+    while i <= num_folders:
+        print("Creating folder {} of {}".format(i, num_folders))
+        
+        #Skip current folder creation upon error
+        try: os.mkdir("./"+ folder_name + "" + str(i))
+        except: i=i+1
+        i=i+1
+
+    input("Press enter to exit")
+
+if __name__ == "__main__":
+        folder_name = set_folder_name()
+        no_folders = num_folders_to_create()
+        create_folders(folder_name, no_folders)
